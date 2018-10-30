1a6f52f39b698caea5f236d489130484ea2bfb6c
Sufyan Muhammad <sufyanm@ontrack.app>
2018-10-30 23:09:31 +1100

    Fix more Ansible version issues

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ yum -y update
 # Install Ansible 2.6.5
 yum -y install epel-release
 sed -i -e 's/^enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
-yum -y --enablerepo=epel install python-pip python-devel python pyOpenSSL
-pip install ansible==2.6.5
+yum -y --enablerepo=epel install python-pip python-devel python ansible pyOpenSSL
+pip install --upgrade ansible==2.6.5
 
 # Clone openshift-ansible repo
 git clone https://github.com/openshift/openshift-ansible ~/openshift-ansible
@@ -15,5 +15,5 @@ cd ~/openshift-ansible
 git checkout release-3.10
 
 # Run playbooks to deploy cluster
-ansible-playbook -i ~/openshift/hosts ~/openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i ~/openshift/hosts ~/openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i ~/openshift/hosts ~/openshift-ansible/playbooks/prerequisites.yml \
+  && ansible-playbook -i ~/openshift/hosts ~/openshift-ansible/playbooks/deploy_cluster.yml
